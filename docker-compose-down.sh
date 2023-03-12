7d92f1396b639481a65859f334597aa562a6ce23
hrach.hovakimyan <hrach.hovakimyan@mil.pub>
2023-03-12 13:13:40 +0400

    initialize

---
+++ docker-compose-down.sh
@@ -0,0 +1,3 @@
+    # -f docker-compose.application.yaml \
+export ROOT_PATH=C:\Users\Hrach\Projects\personal\microservices\
+docker compose -f $ROOT_PATH/jenkins/docker-compose.yaml -f $ROOT_PATH/kafka/docker-compose.yaml -f $ROOT_PATH/portainer/docker-compose.yaml -f $ROOT_PATH/redis/docker-compose.yaml down
